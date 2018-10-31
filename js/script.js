deb2d20da2066ca08f6067d20942d988e3359963
fruity4pie <fruity4pie@gmail.com>
2018-10-31 11:26:53 +0200

    init

---
+++ js/script.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+
+	if(location.hash != '') {
+		$('html, body').animate({scrollTop: $(location.hash).offset().top}, 1000);
+		location.hash = '';
+	}
+
+	new WOW().init();
+
+	$(window).scroll(function(e){
+		parallax();
+	});
+
+	function parallax(){
+		var scrolled = $(window).scrollTop();
+		var elm = [$('#n1'), $('#n2'), $('#n3'), $('#n5'), $('#n8')];
+		for(let i = 0; i < elm.length; i++) {
+			if((scrolled < elm[i].outerHeight() + elm[i].offset().top) && (scrolled > elm[i].offset().top / 2)) {
+				elm[i].css('background-position-y',-((scrolled - elm[i].offset().top) * 0.2 )+'px');
+			}
+		}
+	}
+
+	$(document).bind( 'mousewheel', function (e) {
+    var nt = $(document.body).scrollTop()-(e.deltaY*e.deltaFactor*100);
+
+    $(document.body).stop().animate( {
+         scrollTop : nt
+     } , 1000 );
+} )
+
+	var mobBurg = $('.burger');
+	var fixedMenu = $('.mob-f');
+	mobBurg.on('click', function() {
+		$('.mob-f').toggleClass('active');
+		$('body').toggleClass('active');
+	})
+	fixedMenu.on('click', function() {
+		$(this).toggleClass('active');
+		$('body').toggleClass('active');
+	})
+
+
+	var tabsItem = $('.faq-item');
+	tabsItem.on('click', function(e) {
+		$(this).toggleClass('active');
+	})
+
+	var tabsBtns = $('.bg-tabs li');
+	var bgImg = $('.img-wrap .img-wrap-c');
+
+	tabsBtns.on('click', function(e) {
+		e.preventDefault();
+		tabsBtns.removeClass('active');
+		$(this).addClass('active');
+		let ind = $(this).index();
+
+		bgImg.find('.active').removeClass('active');
+		bgImg.find(`img:eq(${ind})`).addClass('active');
+	})
+
+	//ScrollToSection
+	$('a[href^="#"]').on('click', function(event) {
+	   var target = $(this.getAttribute('href'));
+	   if( target.length ) {
+	     event.preventDefault();
+	     $('html, body').stop().animate({
+	       scrollTop: target.offset().top
+	     }, 1500);
+	   }
+	});
+
+	$('.programs-slider').owlCarousel({
+		loop:false,
+		margin:30,
+		nav:false,
+		navText: [`<img src=${vars.base_url + "/assets/img/arrow.png"} alt="arrow">`, `<img src=${vars.base_url + "/assets/img/arrow.png"} alt="arrow">`],
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				margin: 0,
+				nav: true,
+				dots: true,
+			},
+			501: {
+				items: 2,
+				nav:false
+			},
+			850: {
+				items: 3,
+				nav:false
+			},
+			1179: {
+				items: 4,
+				nav:false
+			},
+		}
+	})
+
+	$('.reviews-slider').owlCarousel({
+		loop:false,
+		margin:30,
+		nav: true,
+		dots: false,
+		navText: [`<img src=${vars.base_url + "/assets/img/arrow.png"} alt="arrow">`, `<img src=${vars.base_url + "/assets/img/arrow.png"} alt="arrow">`],
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				margin: 0,
+			},
+			1100: {
+				items: 2,
+			},
+		}
+	})
+
+	$(window).load(function() {
+   $('.preloader').removeClass('active');
+	 $('body').removeClass('active');
+});
+
+});
+
+$('#videoModal').on('show.bs.modal', function(e) {
+	$('#videoModal .modal-body iframe')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*')
+});
